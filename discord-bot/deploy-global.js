d173620e8bd81aa84d1991365137c5e3aaa88025
Max Kilian <mbj.kilian@outlook.de>
2022-01-27 15:13:25 +0100

    sucessfully added ✨deploy global✨, so i can be lazy again *blush *

---
+++ discord-bot/deploy-global.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { clientId, token } = require("./config.json");
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+
+const commands = [];
+const commandFolders = fs.readdirSync("./commands")
+
+for (const dir of commandFolders){
+	if (dir.startsWith("glob") == true){
+		const commandFiles = fs.readdirSync(`./commands/${dir}`).filter(file => file.endsWith(".js"));
+
+		for (const file of commandFiles) {
+			const command = require(`./commands/${dir}/${file}`);
+		commands.push(command.data.toJSON());
+		}
+	}
+}
+
+client.login(token);
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+client.once("ready", () => {
+	const Guilds = client.guilds.cache.map(guild => `${guild.id}`)
+
+	for (const x in Guilds){
+		rest.put(Routes.applicationGuildCommands(clientId, x), { body: commands })
+			.then(() => console.log("Successfully registered application commands."))
+			.catch(console.error);}
+});
