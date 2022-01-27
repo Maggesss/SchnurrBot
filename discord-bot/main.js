f303f6580600de82b00bad2103ccea495c8e2a2a
Max Kilian <mbj.kilian@outlook.de>
2022-01-27 09:41:30 +0100

    added loop trough folders -> custom commands easily possible

--- discord-bot/main.js
+++ discord-bot/main.js
@@ -5,11 +5,17 @@ const { token } = require("./config.json");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+const commandFolders = fs.readdirSync("./commands")
+
+for (const dir of commandFolders){
+
+	const commandFiles = fs.readdirSync(`./commands/${dir}`).filter(file => file.endsWith(".js"));
+
+	for (const file of commandFiles) {
+		const command = require(`./commands/${dir}/${file}`);
+		client.commands.set(command.data.name, command);
+	}
 }
 
 client.once("ready", () => {
