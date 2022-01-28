9efff213e1e5afd636fa3d88ff85151d616b9e85
Max Kilian <mbj.kilian@outlook.de>
2022-01-28 13:55:12 +0100

    work needed.

--- discord-bot/commands/lin-custom/afk.js
+++ discord-bot/commands/lin-custom/afk.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const fs = require("fs")
+let user = JSON.parse(fs.readFileSync("./commands/lin-custom/afk.json", "utf8"));
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,18 +12,15 @@ module.exports = {
             .setRequired(false)),
 	async execute(interaction) {
         try {
-            const afkPath = "./commands/lin-custom/afk.json"
-            const afkRead = fs.readFileSync(afkPath);
-            const afkFile = JSON.parse(afkRead);
-            const userId = interaction.client.id;
-            if (!afkFile[userId]) {
-                afkFile[userId] = {afk: true};
-                fs.writeFileSync(afkPath, JSON.stringify(afkFile, null, 2));
-            } else {
-                var afkVar = Boolean(true)
-                afkFile[userId] = {afk: afkVar};
-                fs.writeFileSync(afkPath, JSON.stringify(afkFile, null, 2));
-                        }
+            if (!user[message.author.id]) { user[message.author.id] = {
+                afk: true,
+              };
+            }
+            let userData = user[message.author.id];
+            if (userData.afk = true) {
+                userData.afk = false
+            } else {userData.afk = true}
+
         } catch (error) {
             console.error(error);
             return interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
