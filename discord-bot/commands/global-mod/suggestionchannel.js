3d3367e09cbc87991d71fcf81b22e0033790e46e
Maggesss <mbj.kilian@outlook.de>
2022-01-31 17:24:33 +0100

    added permission filter for suggestionchannel

--- discord-bot/commands/global-mod/suggestionchannel.js
+++ discord-bot/commands/global-mod/suggestionchannel.js
@@ -13,9 +13,10 @@ module.exports = {
             .setRequired(true)),
 	async execute(interaction) {
 		const channel = interaction.options.getString("channel");
-        if (fs.existsSync(path.resolve('./data/server/' + interaction.guild.id + '.json'))) {
-                fs.writeFileSync(path.resolve('./data/server/' + interaction.guild.id + '.json'), new Server({ id: interaction.guild.id, suggestionChannelID: channel, name: interaction.guild.name}).toString())
-                return interaction.reply({ content: `Suggestionchannel set.`, ephemeral: true })
-        }
+        if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS)) {
+            if (fs.existsSync(path.resolve('./data/server/' + interaction.guild.id + '.json'))) {
+                    fs.writeFileSync(path.resolve('./data/server/' + interaction.guild.id + '.json'), new Server({ id: interaction.guild.id, suggestionChannelID: channel, name: interaction.guild.name}).toString())
+                    return interaction.reply({ content: `Suggestionchannel set.`, ephemeral: true })
+        }} else { return interaction.reply("You don't have permissions to do that!")}
     }
 };
