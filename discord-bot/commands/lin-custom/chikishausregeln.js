ade266f5d06be326aca528d436dcd32feb6be161
Maggesss <mbj.kilian@outlook.de>
2022-01-31 20:51:26 +0100

    hotfix for hotfix chikishausregeln

--- discord-bot/commands/lin-custom/chikishausregeln.js
+++ discord-bot/commands/lin-custom/chikishausregeln.js
@@ -14,14 +14,14 @@ module.exports = {
         const regelNum = interaction.options.getString("regelNR")
         let Hausregel = ""
         if (regelNum == 1) {Hausregel = "Das Schwängern oder Schwanger werden vor dem Alter von 18 Jahren ist in diesem Haushalt untersagt! Ab dem erhalten der Volljährigkeit geht mir das am Arsch vorbei tbh."}
-        else if (regelNum == 2) {Hausregel = "Es ist mir lieber, wenn ihr illegales zuhause macht. Außerhalb der eigenen vier Wände können viele Unfälle passieren."}
-        else if (regelNum == 3) {Hausregel = "Sexorgien müssen ab 5,43 Personen angekündigt werden. Verhüten nicht vergessen!"}
-        else if (regelNum == 4) {Hausregel = "Unge Fanart jeglicher Art wird aufgehängt."}
-        else if (regelNum == 5) {Hausregel = "Denniz ist ein Hurensohn (nigs gegen seine Mam)"}
-        else if (regelNum == 6) {Hausregel = "Auch wenn Sushi oft sehr nervtötend und gemein sein kann, ist er teilweise echt fucking wholesome."}
-        else if (regelNum == 7) {Hausregel = "Beim Trinken von Alkohol muss mindestens eine Runde Ich-Hab-Noch-Nie gespielt werden!"}
-        else if (regelNum == 8) {Hausregel = "RADLERVERBOT!"}
-        else if (regelNum == 9) {Hausregel = "Menschen dürfen gegessen werden :3"}
+        else if (regelNum == "2") {Hausregel = "Es ist mir lieber, wenn ihr illegales zuhause macht. Außerhalb der eigenen vier Wände können viele Unfälle passieren."}
+        else if (regelNum == "3") {Hausregel = "Sexorgien müssen ab 5,43 Personen angekündigt werden. Verhüten nicht vergessen!"}
+        else if (regelNum == "4") {Hausregel = "Unge Fanart jeglicher Art wird aufgehängt."}
+        else if (regelNum == "5") {Hausregel = "Denniz ist ein Hurensohn (nigs gegen seine Mam)"}
+        else if (regelNum == "6") {Hausregel = "Auch wenn Sushi oft sehr nervtötend und gemein sein kann, ist er teilweise echt fucking wholesome."}
+        else if (regelNum == "7") {Hausregel = "Beim Trinken von Alkohol muss mindestens eine Runde Ich-Hab-Noch-Nie gespielt werden!"}
+        else if (regelNum == "8") {Hausregel = "RADLERVERBOT!"}
+        else if (regelNum == "9") {Hausregel = "Menschen dürfen gegessen werden :3"}
         else {return interaction.reply("Not a valid Rule-Number!")} 
 
 		const embed = new MessageEmbed()
