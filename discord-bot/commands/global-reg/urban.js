c38b955109af10afd9a899ed254133cada81d43f
Maggesss <mbj.kilian@outlook.de>
2022-02-03 23:05:13 +0100

    added emojis

--- discord-bot/commands/global-reg/urban.js
+++ discord-bot/commands/global-reg/urban.js
@@ -11,7 +11,7 @@ module.exports = {
 		const term = interaction.options.getString("search");
 		const query = new URLSearchParams({ term });
 
-		const { list } = await fetch("https://api.urbandictionary.com/v0/define?${query}")
+		const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`)
 			.then(response => response.json());
 		
             if (!list.length) {
@@ -29,7 +29,7 @@ module.exports = {
         .addFields(
             { name: "Definition", value: trim(answer.definition, 1024) },
             { name: "Example", value: trim(answer.example, 1024) },
-            { name: "Rating", value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.` },
+            { name: "Rating", value: `| ${answer.thumbs_up} :thumbsup: | ${answer.thumbs_down} :thumbsdown: |` },
 	    );
             return interaction.reply({ embeds: [embed] });
 	},
