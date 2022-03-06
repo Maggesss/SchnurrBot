52f90e6152dad28cb7bb508dce0ff0bd1a56ca7e
Maggesss <mbj.kilian@outlook.de>
2022-03-06 19:37:57 +0100

    ejfweofa

---
+++ discord-bot/runDeploy.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+const exec = require("child_process").exec
+
+const async = require("async")
+
+const scriptsFolder = "./deployScripts/"
+
+const files = fs.readdirSync(scriptsFolder)
+const funcs = files.map(function(file) {
+  return exec.bind(null, `node ${scriptsFolder}${file}`)
+})
+
+function getResults(err, data) {
+  if (err) {
+    return console.log(err)
+  }
+  const results = data.map(function(lines){
+    return lines.join("")
+  })
+  console.log(results)
+}
+
+async.series(funcs, getResults)
