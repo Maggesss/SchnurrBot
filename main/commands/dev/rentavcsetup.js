45b58787263d9b790f71521fc5ba15011663e3bb
Maggesss <mbj.kilian@outlook.de>
2022-03-13 19:11:08 +0100

    fixed rent a vc thingie! works now!

--- main/commands/dev/rentavcsetup.js
+++ main/commands/dev/rentavcsetup.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require('discord.js');
-const path = require("path")
-const fs = require("fs")
+const path = require("path");
+const fs = require("fs");
 const Server = require("../../source/server/index");
 
 module.exports = {
@@ -12,16 +12,16 @@ module.exports = {
     async execute(interaction) {
         try {
             if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_CHANNELS) || (interaction.user.id == "444460699025014784")) {
-                console.log("Check 1")
-                if (fs.existsSync(path.resolve(`../../data/server/${interaction.guild.id}/regData.json`))) {
-                    console.log("Check 2")
-                    const server = new Server(JSON.parse(fs.readFileSync(path.resolve(`../../data/server/${interaction.guild.id}/regData.json`))));
-                    console.log("Check 3")
+                console.log(path.resolve(`./data/server/${interaction.guild.id}/regData.json`))
+                if (fs.existsSync(path.resolve(`./data/server/${interaction.guild.id}/regData.json`))) {
+                    const server = new Server(JSON.parse(fs.readFileSync(path.resolve(`./data/server/${interaction.guild.id}/regData.json`))));
                     const newChannel = await interaction.guild.channels.create("rent-a-vc", { type: "GUILD_VOICE" });
-                    console.log("Check 4")
-                    fs.writeFileSync(path.resolve(`../../data/server/${interaction.guild.id}/regData.json`), new Server({ id: interaction.guild.id, name: interaction.guild.name, rentavcChannelID: newChannel.id, suggestionChannelID: server.suggestionChannelID}).toString());
-                    console.log("Check 5")
-                    return interaction.reply({ content: `Channel created.`, ephemeral: true });
+                    fs.writeFileSync(path.resolve(`./data/server/${interaction.guild.id}/regData.json`), new Server({ id: interaction.guild.id, name: interaction.guild.name, rentavcChannelID: newChannel.id, suggestionChannelID: server.suggestionChannelID}).toString());
+                    
+                    interaction.client.channels.fetch("950064195464986725").then((channel) => {
+                        channel.send(`\`\`${interaction.member}\`\` created new rent-a-vc channel on server: \`\`${interaction.guild.name}\`\``)
+                        return interaction.reply({ content: `Channel created.`, ephemeral: true });
+                    });
                 };
             } else { return interaction.reply({ content: `You don't have permissions to do that!`, ephemeral: true }) };
         } catch (error) {
