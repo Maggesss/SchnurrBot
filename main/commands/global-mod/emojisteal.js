a07fc5be21eeb798bf5e0db09651f4861784126e
Magges <mbj.kilian@outlook.de>
2022-09-13 16:49:38 +0200

    emotesteal

--- main/commands/global-mod/emojisteal.js
+++ main/commands/global-mod/emojisteal.js
@@ -5,19 +5,24 @@ const functions = require("../../functions")
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("emojisteal")
-		.setDescription("steal an emoji from some other server")
+		.setDescription("steal an not animated emoji from some other server")
 		.addStringOption(option => option
             .setName("emoji")
             .setDescription("emoji of your choice")
-            .setRequired(true)),
+            .setRequired(true))
+		.addStringOption(option => option
+			.setName("name")
+			.setDescription("possible prefix infront of emoji name")
+			.setRequired(true)),
 		
 	async execute(interaction) {
 		const emoji = interaction.options.getString("emoji");
+		const name = interaction.options.getString("name");
 		try {
             if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS) || (functions.isHelper(interaction.user.id) == true)) {
-				console.log(emoji.match(/\d+/)[0])
-				interaction.reply("Sucessfull.")
-				
+				//emoji.match(/\d+/)[0]
+				await interaction.guild.emojis.create(`https://cdn.discordapp.com/emojis/${emoji.match(/\d+/)[0]}.png`, name);
+				interaction.reply(`Added emoji to server-emojis.`);
 			} else { return interaction.reply("You don't have permissions to do that!") };
         } catch (error) {
             console.error(error);
